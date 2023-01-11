64920aa5de8a61a4cec6e233cf17badd21649f35
muratirmak <mrtriver@gmail.com>
2023-01-11 13:14:44 -0500

    base application App

---
+++ lecture-1-review-with-todo-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <TodoForm />
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
+
+function TodoForm(){
+return <div></div>
+}
+
+function TodoList(){
+return <div></div>
+}
